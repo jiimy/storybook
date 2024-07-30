e157bf929972e8fb3a6b7a79f82bfb0d2cfdf3b3
rudwnok <rudwnok123@naver.com>
2024-07-30 16:43:33 +0900

    default 타입. next, prev 기능 추가

--- src/stories/pagination/Pagination.stories.tsx
+++ src/stories/pagination/Pagination.stories.tsx
@@ -7,6 +7,7 @@ const meta: Meta<typeof Pagination> = {
   title: "Component/Pagination",
   component: Pagination,
   parameters: {
+    // componentSubtitle: 'dd',
   },
   tags: ["autodocs"],
   argTypes: {
@@ -16,25 +17,56 @@ const meta: Meta<typeof Pagination> = {
 export default meta;
 type Story = StoryObj<typeof Pagination>;
 
-export const Auto: Story = {
-  // args: {
-  //   item: ['목록1', '목록2'],
-  // },
+export const NoPagingApi: Story = {
   render: function Render(args) {
-    const URL = 'https://jsonplaceholder.typicode.com/posts';
+    const [post, setPosts] = useState([]); // 넘겨줄 데이터의 length
+    const [index, setIndex] = useState(1);
 
-    const [post, setPosts] = useState([]);
+    const viewPageCount = 10;
 
+    // 1. 페이징 처리가 안되어있는 api 일 경우
     useEffect(() => {
+      console.log('dindex', index);
       axios
-        .get(URL)
-        .then((res) => setPosts(res.data.slice(0, 24)))
-    }, []);
+        .get('https://jsonplaceholder.typicode.com/todos')
+        .then((res) => {
+          // index === 1 && setPosts(res.data.slice(index, index * viewPageCount))
+          index === 1 ? setPosts(res.data.slice((index - 1), index * viewPageCount))
+            : setPosts(res.data.slice((index - 1) * 10, index * 10))
+        })
+    }, [index]);
+
+    console.log('데이터', post);
 
     return (
       <>
         데이터
-        <Pagination totalPost={24} />
+        <Pagination theme="auto" totalPost={202} setCurrentPage={setIndex} currentPage={index} />
+      </>
+    )
+  }
+}
+
+export const PagingApi: Story = {
+  render: function Render(args) {
+
+    const [post, setPosts] = useState([]); // 넘겨줄 데이터의 length
+    const [index, setIndex] = useState(1);
+
+    const viewPageCount = 10;
+
+    // 1. 페이징 처리가 안되어있는 api 일 경우
+    useEffect(() => {
+      console.log('dindex', index);
+      axios
+        .get(`https://jsonplaceholder.typicode.com/photos?_page=${index}&_limit=${viewPageCount}`)
+        .then((res) => console.log('dd', res.data))
+    }, [index]);
+
+
+    return (
+      <>
+        <Pagination theme="default" totalPost={24} setCurrentPage={setIndex} currentPage={index} btnRange={10} />
       </>
     )
   }
--- src/stories/pagination/Pagination.tsx
+++ src/stories/pagination/Pagination.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../button/Button';
 import s from './pagination.module.scss';
+import styled from 'styled-components';
+import classNames from 'classnames';
 
 type paginationType = {
-  theme?: 'auto' | 'default', // auto일 경우 항상 선택한 번호가 가운데를 넘지 않음. viewNumber 가 
-  // viewNumber?: 5 | 10 | number, // 한번에 보여줄 버튼 갯수
+  theme?: 'auto' | 'default', // auto일 경우 항상 선택한 번호가 가운데를 넘지 않음. default 일 경우 마지막 번호를 선택하면 자동으로 다음 번호가 나오지 않음. 
   isButton?: boolean,
 
-  // pageIndex: React.Dispatch<React.SetStateAction<number>>, // 현재 선택된 index
-  // pageRange: number, // 페이지당 보여줄 갯수
+  setCurrentPage: (index: number) => void, // 현재 선택된 index
+  currentPage: number, // 현재 페이지
+  btnRange?: 5 | 10 | number, // 한번에 보여줄 버튼 갯수 - 미사용됨
+  pageRange?: number, // 페이지당 보여줄 갯수 - 
   totalPost: number, // 총 갯수
 }
 
@@ -16,52 +19,95 @@ type paginationType = {
  * theme - auto : 5개 이고 auto이면 선택한 index가 4이상일때 선택한 index가 가운데 (ex. 3선택 -> 번호이동 없음. 번호는 12345, 4선택 -> 번호이동 있음. 번호는 23456
  * theme - default 
  */
-const Pagination = ({ theme = 'auto',
-  // viewNumber = 5, 
+const Pagination = ({
+  theme = 'auto',
   isButton = true,
+  btnRange = 10,
+  pageRange = 10,
+  currentPage,
   totalPost,
-  // pageIndex, 
-  ...props }: paginationType) => {
+  setCurrentPage,
+  ...props
+}: paginationType) => {
+  const [currentBtn, setCurrentBtn] = useState(currentPage); // 현재 선택한 버튼 - 사용중
+  const [array, setArray] = useState<number[]>([]); // auto일때 사용.
+  const btnGroupIndex = Math.ceil(currentBtn / btnRange);
+  const startPage = (btnGroupIndex - 1) * btnRange + 1;
 
-  const pageNumbers = [];
-  for (let i = 1; i <= totalPost; i++) {
-    pageNumbers.push(i);
+  const PrevButton = () => {
+    return (<Button onClick={() => setCurrentBtn(prev => prev - 1)}>{'<'}</Button>)
+  }
+  const NextButton = () => {
+    return (<Button onClick={() => setCurrentBtn(prev => prev + 1)}>{'>'}</Button>)
   }
 
-  console.log('받은 데이터', totalPost);
-  // const itemCount =
+  useEffect(()=> {
+    setCurrentPage(currentBtn);
+    console.log(`현재 페이지수 ${currentBtn} 그룹 ${btnGroupIndex}` )
+  }, [currentBtn])
 
-  // return (
-  //   <div>
-  //     <Button >{'<<'}</Button>
-  //     <Button>{'<'}</Button>
-  //     페이지네이션
-  //     <ul>
-  //       {totalPost.map((item: any[], index: number) => <li>
-
-  //       </li>)}
-  //     </ul>
-  //     <Button>{'>'}</Button>
-  //     <Button>{'>>'}</Button>
-  //   </div>
-  // );
   return (
     <>
-      <ul className={s.container}>
-        {pageNumbers.map((item: any, index: number) => (
-          <li
-            key={index}
-            onClick={() => {
-              // console.log("클릭", pageNumber[name]);
-            }}
-          >
-            <span>{item}</span>
-          </li>
-        ))}
-      </ul>
+    {theme === 'auto' && 
+      <div className={s.pagination}>
+        <Button >{'<<'}</Button>
+        <PrevButton/>
+        <div>
+            {array.map((item, index) => 
+              <Button onClick={() => setCurrentPage(index + 1)}
+                theme='secondary'
+                // TODO: 이 부분 styled로 할지 scss로 할지 고민중
+                className={classNames('', {
+                  [s.is_select]: currentBtn === index + 1
+                })} key={index} >{index + 1}</Button>
+            )}
+        </div>
+          <NextButton />
+        <Button>{'>>'}</Button>
+      </div>
+    }
+      {theme === 'default' &&
+        <div className={s.pagination}>
+          <Button >{'<<'}</Button>
+          <PrevButton/>
+          <div>
+            {/* {Array.from({ length: pageRange }, (_, index) => (
+              <Button key={index} onClick={() => setCurrentBtn(index + 1)}
+                theme='secondary'
+                className={s.is_select}
+              >{index + 1}</Button>
+            ))} */}
+            {Array(btnRange)
+              .fill(startPage)
+              .map((_, index) => {
+                return (
+                  <Button
+                    key={index}
+                    theme='secondary'
+                    onClick={() => setCurrentBtn(startPage + index)
+                    }
+                  >
+                    {startPage + index}
+                  </Button>
+                );
+              })}
+          </div>
+          <NextButton />
+          <Button>{'>>'}</Button>
+        </div>
+      }
     </>
   );
 };
 
 export default Pagination;
 export type { paginationType }
+
+
+// const StartButton = () => {
+
+// }const StartButton = () => {
+
+// }const StartButton = () => {
+  
+// }
